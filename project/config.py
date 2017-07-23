3ed26a83ffddc136e9cd38ebde4ec6f0c3a15a78
Dag Stromsvag <dag.stromsvag@gmail.com>
2017-07-23 13:54:34 +0200

    Add 'Getting Started'

---
+++ project/config.py
@@ -0,0 +1,17 @@
+class BaseConfig():
+    """Base configuration"""
+    DEBUG = False
+    TESTING = True
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
